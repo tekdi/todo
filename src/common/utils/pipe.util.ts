864c20bcd8b2a693b9209e5ca50096d3184af764
poojakarma <pooja.karma@tekditechnologies.com>
2024-09-25 13:26:11 +0530

    filter todo API

---
+++ src/common/utils/pipe.util.ts
@@ -0,0 +1,25 @@
+import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common';
+
+@Injectable()
+export class TodoDateValidationPipe implements PipeTransform {
+    transform(createTodoDto: any) {
+        const currentDate = new Date();
+        const startDate = createTodoDto.start_date ? new Date(createTodoDto.start_date) : null;
+        const dueDate = createTodoDto.due_date ? new Date(createTodoDto.due_date) : null;
+
+        // Check for invalid start date or due date (should be greater than current date)
+        if (startDate && startDate <= currentDate) {
+            throw new BadRequestException('Start date must be greater than the current date and time');
+        }
+
+        if (dueDate && dueDate <= currentDate) {
+            throw new BadRequestException('Due date must be greater than the current date and time');
+        }
+
+        // Check if the due date is before the start date
+        if (startDate && dueDate && dueDate < startDate) {
+            throw new BadRequestException('Due date cannot be earlier than start date');
+        }
+        return createTodoDto;
+    }
+}
--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -12,25 +12,28 @@ import {
   ApiBadRequestResponse,
   ApiBody,
   ApiCreatedResponse,
+  ApiOkResponse,
   ApiTags,
 } from "@nestjs/swagger";
 import { TodoService } from "./todo.service";
 import { CreateTodoDto } from "./dto/create-todo.dto";
 import { Response } from "express";
 import { AllExceptionsFilter } from "src/common/utils/exception.filter";
-import { API_ID } from "src/common/utils/constants.util";
+import { API_ID, ERROR_MESSAGES, SUCCESS_MESSAGES } from "src/common/utils/constants.util";
+import { FilterRequestDTO } from "./dto/list-todo.dto";
+import { TodoDateValidationPipe } from "src/common/utils/pipe.util";
 
 @Controller("todo")
 @ApiTags("Todo")
 export class TodoController {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(private readonly todoService: TodoService) { }
 
   @UseFilters(new AllExceptionsFilter(API_ID.CREATE_TODO))
   @Post("/create")
-  @UsePipes(new ValidationPipe({ transform: true }))
-  @ApiCreatedResponse({ description: "toDo created" })
+  @UsePipes(new ValidationPipe({ transform: true }), new TodoDateValidationPipe())
+  @ApiCreatedResponse({ description: SUCCESS_MESSAGES.TODO_CREATED })
   @ApiBody({ type: CreateTodoDto })
-  @ApiBadRequestResponse({ description: "Bad request" })
+  @ApiBadRequestResponse({ description: ERROR_MESSAGES.BAD_REQUEST })
   async createTo(
     @Body() createTodoDto: CreateTodoDto,
     @Res() response: Response,
@@ -38,9 +41,15 @@ export class TodoController {
     return await this.todoService.createTodo(createTodoDto, response);
   }
 
-  @Post("/list")
-  async viewList() {
-    return true;
+  @UseFilters(new AllExceptionsFilter(API_ID.LIST_TODO))
+  @Post('/list')
+  @UsePipes(new ValidationPipe({ transform: true }))
+  @ApiOkResponse({ description: SUCCESS_MESSAGES.TODO_LIST })
+  @ApiBadRequestResponse({ description: ERROR_MESSAGES.BAD_REQUEST })
+  @ApiBody({ type: FilterRequestDTO })
+  async viewList(@Res() response: Response, @Body() filterRequestDTO: FilterRequestDTO
+  ) {
+    return await this.todoService.viewListTodo(filterRequestDTO, response);
   }
 
   @Patch("/:id")
